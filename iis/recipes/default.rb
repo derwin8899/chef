833fb16f2a6fadcb310e94041e843e1336f20c7d
derwin8899 <derwin8899@gmail.com>
2023-08-06 11:30:16 -0700

    Update default.rb

--- iis/recipes/default.rb
+++ iis/recipes/default.rb
@@ -9,19 +9,23 @@ service 'iis' do
   action [:enable, :start]
 end
 
+# Loop through each application block in attributes dir to create/enforce resrouces.
 node['iis_applications'].each do |app|
 
+  # Create app pool.
   iis_pool "#{app['app_pool_name']}" do
     periodic_restart_schedule "#{app['restart_time']}"
     action [:add, :start, :config]
   end
-    
+
+  # Create site with root directory and binding. 
   iis_site "#{app['site_name']}" do
     path "#{app['site_path']}"
     port "#{app['site_port']}"
     application_pool "#{app['site_application_pool']}"
   end
-  
+
+  # Copy files from files dir in cookbook to root directory on target server.
   remote_directory "#{app['site_path']}" do
     source "#{app['site_name']}"
     action :create
